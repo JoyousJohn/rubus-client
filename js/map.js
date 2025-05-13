cc5e5a90a971b1f7d5a56940bd458e9c8a874b47
John <mrjohnishappy@gmail.com>
2025-05-13 03:50:32 -0400

    Only hide stops grid, not wrapper, if invalid ETAs

--- js/map.js
+++ js/map.js
@@ -1450,7 +1450,7 @@ function popInfo(busId, resetCampusFontSize) {
             }, 0);
             
         } else {
-            $('.info-next-stops').hide(); // For some reason *only* the closest stop at top of next stops remains visible if negative ETA, and only if negative ETA happens while site was open. Investigate why, unsure if this fixes. The closest stop should be part of the element, so I'm confused...
+            $('.next-stops-grid').hide(); // For some reason *only* the closest stop at top of next stops remains visible if negative ETA, and only if negative ETA happens while site was open. Investigate why, unsure if this fixes. The closest stop should be part of the element, so I'm confused...
         }
     }
 
