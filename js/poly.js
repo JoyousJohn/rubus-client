4f6e4cbf5514006282567f5014902ae7138efd1d
John <mrjohnishappy@gmail.com>
2025-05-29 23:58:51 -0400

    Don't allow switching to inactive stop directions

--- js/poly.js
+++ js/poly.js
@@ -403,34 +403,75 @@ async function popStopInfo(stopId) {
 
     if (stopId === 6) { // Hill North
         $('.info-stop-switch').css('display', 'inline-block')
-        $('.stop-name-wrapper').parent().one('click', function() {flyToStop(7)});
         $('.info-stop-switch-1').text('NB').css('color', 'var(--theme-bg)').css('background-color', 'var(--theme-color)');
         $('.info-stop-switch-2').text('SB').css('color', '').css('background-color', '')
+
+        if (!activeStops.includes(7)) {
+            $('.info-stop-switch-2').hide()
+        } else {
+            $('.info-stop-switch-2').show()
+            $('.stop-name-wrapper').parent().one('click', function() {flyToStop(7)});
+        }
+
     } else if (stopId === 7) { // Hill South
         $('.info-stop-switch').css('display', 'inline-block')
-        $('.stop-name-wrapper').parent().one('click', function() {flyToStop(6)});
         $('.info-stop-switch-1').text('NB').css('color', '').css('background-color', '')
         $('.info-stop-switch-2').text('SB').css('color', 'var(--theme-bg)').css('background-color', 'var(--theme-color)');
+    
+        if (!activeStops.includes(6)) {
+            $('.info-stop-switch-1').hide()
+        } else {
+            $('.info-stop-switch-1').show()
+            $('.stop-name-wrapper').parent().one('click', function() {flyToStop(6)});
+        }
+    
     } else if (stopId === 22) { // SoCam North
         $('.info-stop-switch').css('display', 'inline-block')
-        $('.stop-name-wrapper').parent().one('click', function() {flyToStop(23)});
         $('.info-stop-switch-1').text('NB').css('color', 'var(--theme-bg)').css('background-color', 'var(--theme-color)');
         $('.info-stop-switch-2').text('SB').css('color', '').css('background-color', '')
+    
+        if (!activeStops.includes(23)) {
+            $('.info-stop-switch-2').hide()
+        } else {
+            $('.info-stop-switch-2').show()
+            $('.stop-name-wrapper').parent().one('click', function() {flyToStop(23)});
+        }
     } else if (stopId === 23) { // SoCam South
         $('.info-stop-switch').css('display', 'inline-block')
-        $('.stop-name-wrapper').parent().one('click', function() {flyToStop(22)});
         $('.info-stop-switch-1').text('NB').css('color', '').css('background-color', '')
         $('.info-stop-switch-2').text('SB').css('color', 'var(--theme-bg)').css('background-color', 'var(--theme-color)');
+    
+        if (!activeStops.includes(22)) {
+            $('.info-stop-switch-1').hide()
+        } else {
+            $('.info-stop-switch-1').show()
+            $('.stop-name-wrapper').parent().one('click', function() {flyToStop(22)});
+        }
     } else if (stopId === 3) { // SAC North
         $('.info-stop-switch').css('display', 'inline-block')
         $('.stop-name-wrapper').parent().one('click', function() {flyToStop(4)});
         $('.info-stop-switch-1').text('NB').css('color', 'var(--theme-bg)').css('background-color', 'var(--theme-color)');
         $('.info-stop-switch-2').text('SB').css('color', '').css('background-color', '')
+    
+        if (!activeStops.includes(4)) {
+            $('.info-stop-switch-2').hide()
+        } else {
+            $('.info-stop-switch-2').show()
+            $('.stop-name-wrapper').parent().one('click', function() {flyToStop(4)});
+        }
+    
     } else if (stopId === 4) { // SAC South
         $('.info-stop-switch').css('display', 'inline-block')
         $('.stop-name-wrapper').parent().one('click', function() {flyToStop(3)});
         $('.info-stop-switch-1').text('NB').css('color', '').css('background-color', '')
         $('.info-stop-switch-2').text('SB').css('color', 'var(--theme-bg)').css('background-color', 'var(--theme-color)');
+    
+        if (!activeStops.includes(3)) {
+            $('.info-stop-switch-1').hide()
+        } else {
+            $('.info-stop-switch-1').show()
+            $('.stop-name-wrapper').parent().one('click', function() {flyToStop(3)});
+        }
     } else {
         $('.info-stop-switch').hide();
     }
