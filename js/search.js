56270081b34351c35c99fdbc145bb1051eb886ae
John <mrjohnishappy@gmail.com>
2025-08-18 20:11:13 -0400

    Add search aliases

--- js/search.js
+++ js/search.js
@@ -11,17 +11,35 @@ $(document).ready(function() {
     let buildingList = [];
     let fuseReady = false;
 
+    // Alias mapping: main word -> array of aliases
+    const aliasMap = {
+        'recreation': ['gym', 'rec', 'fitness', 'workout'],
+        'library': ['books', 'study', 'reading'],
+        'center': ['building', 'complex'],
+        'hall': ['building'],
+        'athletic': ['gym', 'sports'],
+        'college ave': ['ca'],
+        'livingston': ['livi']
+    };
+
     // Load building_index.json and initialize Fuse.js
     fetch('lib/building_index.json')
         .then(response => response.json())
         .then(data => {
-            // Convert object to array with name property
-            buildingList = Object.keys(data).map(name => ({
-                name: name,
-                ...data[name]
-            }));
+            // Convert object to array with name property and inject aliases
+            buildingList = Object.keys(data).map(name => {
+                const obj = { name: name, ...data[name] };
+                obj.aliases = obj.aliases || [];
+                // Inject aliases based on aliasMap
+                for (const mainWord in aliasMap) {
+                    if (obj.name.toLowerCase().includes(mainWord)) {
+                        obj.aliases = obj.aliases.concat(aliasMap[mainWord]);
+                    }
+                }
+                return obj;
+            });
             fuse = new Fuse(buildingList, {
-                keys: ['name'],
+                keys: ['name', 'aliases'],
                 threshold: 0.3,
                 includeScore: true,
             });
@@ -39,7 +57,24 @@ $(document).ready(function() {
             return;
         }
         $('.search-results-wrapper, .search-results').show();
-        const results = fuse.search(query);
+
+        // Extended search: require all tokens to match in any field
+        const tokens = query.split(/\s+/).filter(Boolean);
+        let results;
+        if (tokens.length > 1) {
+            const extendedQuery = {
+                $and: tokens.map(token => ({
+                    $or: [
+                        { name: token },
+                        { aliases: token }
+                    ]
+                }))
+            };
+            results = fuse.search(extendedQuery);
+        } else {
+            results = fuse.search(query);
+        }
+
         if (results.length === 0) {
             $results.html('<div class="dimgray">No buildings found.</div>');
             return;
@@ -54,12 +89,8 @@ $(document).ready(function() {
             $elm = $(`<div class="search-result-item flex">${icon}<div>${item.name}</div></div>`);
             $elm.click(function() {
                 closeSearch();
-                console.log('item', item);
                 showBuildingInfo(item);
-
-                map.flyTo([item.lat, item.lng], 17, {
-                    duration: 0.3
-                });
+                map.flyTo([item.lat, item.lng], 17, { duration: 0.3 });
             });
             $results.append($elm);
         });
